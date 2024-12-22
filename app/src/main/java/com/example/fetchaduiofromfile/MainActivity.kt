51c1aa3d93fe68ab9df4e1f67d64f9a3e5bffb90
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2024-12-22 19:50:44 +0200

    feat:add feature for check permission

--- app/src/main/java/com/example/fetchaduiofromfile/MainActivity.kt
+++ app/src/main/java/com/example/fetchaduiofromfile/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.fetchaduiofromfile
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.DocumentsContract
@@ -9,6 +11,7 @@ import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.fetchaduiofromfile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -23,11 +26,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         initFilePickerLauncher()
         binding.addAudio.setOnClickListener {
-            extractType = 1
-            pickAudioFile()
+            if (checkAudioPermission()) {
+                extractType = 1
+                pickAudioFile()
+            }else{
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) { // Android 13+
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(android.Manifest.permission.READ_MEDIA_AUDIO),
+                        1
+                    )
+                } else {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                        1
+                    )
+                }
+            }
         }
-
     }
+
+
+    private fun checkAudioPermission(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) { // Android 13+
+            ActivityCompat.checkSelfPermission(
+                this, android.Manifest.permission.READ_MEDIA_AUDIO
+            ) == PackageManager.PERMISSION_GRANTED
+
+
+        } else {
+            ActivityCompat.checkSelfPermission(
+                this, android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
 
     private fun initFilePickerLauncher() {
         filePickerLauncher = registerForActivityResult(
@@ -36,19 +70,28 @@ class MainActivity : AppCompatActivity() {
             if (result.resultCode == RESULT_OK) {
                 val selectedAudioUri: Uri? = result.data?.data
                 selectedAudioUri?.let {
-                    val res = getFilePathFromUri(it)
-                    Log.d("MY_TAG", "Audio URI: $res")
-                    if(res == null ){
+                    val filePath = getFilePathFromUri(it)
+                    Log.d("MY_TAG", "Audio URI: $filePath")
+                    if (filePath == null) {
                         Log.d("MY_TAG", "Path of file : is NULL")
 
-                    }else{
-                        Log.d("MY_TAG", "Path of file : $res")
+                    } else {
+                        Log.d("MY_TAG", "Path of file : $filePath")
                     }
 
                 }
             }
         }
     }
+
+
+    private fun pickAudioFile() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "audio/*" // Filter for audio files
+        }
+        filePickerLauncher.launch(Intent.createChooser(intent, "Select an Audio File"))
+    }
+
     fun getFilePathFromUri(uri: Uri): String? {
         if (DocumentsContract.isDocumentUri(this, uri)) {
             val docId = DocumentsContract.getDocumentId(uri)
@@ -63,10 +106,5 @@ class MainActivity : AppCompatActivity() {
         return null
     }
 
-    private fun pickAudioFile() {
-        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
-            type = "audio/*" // Filter for audio files
-        }
-        filePickerLauncher.launch(Intent.createChooser(intent, "Select an Audio File"))
-    }
+
 }
